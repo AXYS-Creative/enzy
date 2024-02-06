29e807574ad286dffd3c13fd91e3b8ae9c69e827
AXYS-Creative <axyscreativeagency@gmail.com>
2024-02-05 20:27:42 -0700

    hero progress

--- javascript/index.js
+++ javascript/index.js
@@ -16,9 +16,9 @@ import "./scroll.js";
 })();
 
 // Get Current Year for Copyright
-(function getCurrentYear() {
-  const yearText = document.querySelector(".year-text");
-  const currentYear = new Date().getFullYear();
+// (function getCurrentYear() {
+//   const yearText = document.querySelector(".year-text");
+//   const currentYear = new Date().getFullYear();
 
-  yearText.innerHTML = currentYear;
-})();
+//   yearText.innerHTML = currentYear;
+// })();
--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,18 +1,58 @@
-const toggleClassOnScroll = () => {
-  const siteHeader = document.querySelector(".site-header");
-  const headerCtaWrapper = document.querySelector(".header-cta-wrapper");
+let mqMinlg = window.matchMedia("(min-width: 1024px)");
+// let mqMaxSm = window.matchMedia("(max-width: 480px)");
+// let mqMouse = window.matchMedia("(hover: hover)");
 
-  const checkScroll = () => {
-    if (window.scrollY >= 96) {
-      siteHeader.classList.add("scroll-active");
-      headerCtaWrapper.setAttribute("aria-hidden", "true");
+const throttle = (func, limit) => {
+  let lastFunc;
+  let lastRan;
+  return function () {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
     } else {
-      siteHeader.classList.remove("scroll-active");
-      headerCtaWrapper.removeAttribute("aria-hidden");
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
     }
   };
-
-  window.addEventListener("scroll", checkScroll);
 };
 
-toggleClassOnScroll();
+const siteHeader = document.querySelector(".site-header"),
+  headerCtaWrapper = document.querySelector(".header-cta-wrapper"),
+  menuBtn = document.querySelector(".menu-btn"),
+  headerCta1 = headerCtaWrapper.querySelector(".cta-1"),
+  headerCta2 = headerCtaWrapper.querySelector(".cta-2");
+
+if (mqMinlg.matches) {
+  menuBtn.setAttribute("tabindex", "-1");
+
+  const toggleClassOnScroll = () => {
+    const checkScroll = () => {
+      if (window.scrollY >= 24) {
+        siteHeader.classList.add("scroll-active");
+        headerCtaWrapper.setAttribute("aria-hidden", "true");
+        menuBtn.setAttribute("aria-hidden", "false");
+        menuBtn.removeAttribute("tabindex");
+        headerCta1.setAttribute("tabindex", "-1");
+        headerCta2.setAttribute("tabindex", "-1");
+      } else {
+        siteHeader.classList.remove("scroll-active");
+        headerCtaWrapper.setAttribute("aria-hidden", "false");
+        menuBtn.setAttribute("aria-hidden", "true");
+        menuBtn.setAttribute("tabindex", "-1");
+        headerCta1.removeAttribute("tabindex");
+        headerCta2.removeAttribute("tabindex");
+      }
+    };
+
+    window.addEventListener("scroll", throttle(checkScroll, 100)); // Throttle checkScroll, adjust 100ms as needed
+  };
+
+  toggleClassOnScroll();
+}
