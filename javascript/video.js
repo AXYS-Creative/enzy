bce3b410950fb06dbd9e9e057541debd1dbadea0
AXYS-Creative <axyscreativeagency@gmail.com>
2024-02-14 20:16:53 -0700

    video functionality updates

--- javascript/video.js
+++ javascript/video.js
@@ -1,18 +1,28 @@
 const enzyVideo = document.querySelector('.enzy-video'),
- videoToggleBtn = document.querySelector('.video-toggle-btn'),
- playIconWrapper = document.querySelector('.play-icon-wrapper'),
- pauseIconWrapper = document.querySelector('.pause-icon-wrapper');
+      videoToggleBtn = document.querySelector('.video-toggle-btn'),
+      playIconWrapper = document.querySelector('.play-icon-wrapper'),
+      pauseIconWrapper = document.querySelector('.pause-icon-wrapper');
 
-videoToggleBtn.addEventListener('click', () => {
-  console.log('clicked video button')
-
+const toggleVideoPlayState = () => {
   if (enzyVideo.paused || enzyVideo.ended) {
     enzyVideo.play();
+    videoToggleBtn.style.opacity = "0";
     playIconWrapper.style.display = "none";
     pauseIconWrapper.style.display = "flex";
   } else {
     enzyVideo.pause();
+    videoToggleBtn.style.opacity = "1";
     playIconWrapper.style.display = "flex";
     pauseIconWrapper.style.display = "none";
+  }
+}
+
+enzyVideo.addEventListener('click', toggleVideoPlayState);
+
+videoToggleBtn.addEventListener('click', toggleVideoPlayState);
+
+enzyVideo.addEventListener('keydown', (e) => {
+  if (e.key === "Enter") {
+    toggleVideoPlayState();
   }
 });
