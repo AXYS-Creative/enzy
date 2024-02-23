2fe2f7fed3118e9bc183809f127d64564a2df229
AXYS-Creative <axyscreativeagency@gmail.com>
2024-02-22 21:09:32 -0700

    faq updates

--- javascript/faq.js
+++ javascript/faq.js
@@ -1,24 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const faqList = document.querySelector(".faq-list");
+const faqList = document.querySelector(".faq-list");
 
-  faqList.addEventListener("click", function (event) {
-    const clickedElement = event.target;
-    const isQuestionOrAnswer =
-      clickedElement.closest(".question") || clickedElement.closest(".answer");
+faqList.addEventListener("click", function (event) {
+  const clickedElement = event.target;
+  const isQuestionOrAnswer =
+    clickedElement.closest(".question") || clickedElement.closest(".answer");
 
-    if (isQuestionOrAnswer) {
-      const faqItem = clickedElement.closest(".faq-item");
-      toggleFAQItem(faqItem);
-    }
-  });
-
-  function toggleFAQItem(faqItem) {
-    faqItem.classList.toggle("active");
-
-    document.querySelectorAll(".faq-item").forEach((item) => {
-      if (item !== faqItem) {
-        item.classList.remove("active");
-      }
-    });
+  if (isQuestionOrAnswer) {
+    const faqItem = clickedElement.closest(".faq-item");
+    toggleFAQItem(faqItem);
   }
 });
+
+function toggleFAQItem(faqItem) {
+  faqItem.classList.toggle("active");
+
+  // Toggle this block to allow/prevent multiple faq items open at the same time
+  // document.querySelectorAll(".faq-item").forEach((item) => {
+  //   if (item !== faqItem) {
+  //     item.classList.remove("active");
+  //   }
+  // });
+}
