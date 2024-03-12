d8b95f455e4e108056be824faafb43eb212e5b44
AXYS-Creative <axyscreativeagency@gmail.com>
2024-03-12 08:32:27 -0600

    hero subtext & video btn on mobile

--- javascript/video.js
+++ javascript/video.js
@@ -1,10 +1,12 @@
-const enzyVideo = document.querySelector('.enzy-video'),
-      videoToggleBtn = document.querySelector('.video-toggle-btn'),
-      playIconWrapper = document.querySelector('.play-icon-wrapper'),
-      pauseIconWrapper = document.querySelector('.pause-icon-wrapper');
+// let minSm = window.matchMedia("(min-width: 480px)");
+let mqTouch = window.matchMedia("(hover: none)");
+
+const enzyVideo = document.querySelector(".enzy-video"),
+  videoToggleBtn = document.querySelector(".video-toggle-btn"),
+  playIconWrapper = document.querySelector(".play-icon-wrapper"),
+  pauseIconWrapper = document.querySelector(".pause-icon-wrapper");
 
 const toggleVideoPlayState = () => {
-  
   if (enzyVideo.paused || enzyVideo.ended) {
     enzyVideo.play();
     videoToggleBtn.style.opacity = "0";
@@ -16,13 +18,25 @@ const toggleVideoPlayState = () => {
     playIconWrapper.style.display = "flex";
     pauseIconWrapper.style.display = "none";
   }
-}
+};
 
-enzyVideo.addEventListener('click', toggleVideoPlayState);
-videoToggleBtn.addEventListener('click', toggleVideoPlayState);
+enzyVideo.addEventListener("click", toggleVideoPlayState);
+videoToggleBtn.addEventListener("click", toggleVideoPlayState);
 
-enzyVideo.addEventListener('keydown', (e) => {
+enzyVideo.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     toggleVideoPlayState();
   }
 });
+
+// Remove on mobile
+
+const watchQueryTouch = () => {
+  videoToggleBtn.addEventListener("click", () => {
+    videoToggleBtn.style.display = "none";
+  });
+};
+
+mqTouch.addEventListener("change", watchQueryTouch);
+
+watchQueryTouch(mqTouch);
