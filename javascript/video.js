7b1cd0e99cce4a2cb3834f066a337994dc8e22d2
AXYS-Creative <axyscreativeagency@gmail.com>
2024-03-18 20:37:59 -0600

    aspect ratio and pause video on scroll

--- javascript/video.js
+++ javascript/video.js
@@ -29,9 +29,27 @@ enzyVideo.addEventListener("keydown", (e) => {
 });
 
 // Remove on touch screen devices
-
 if (mqTouch.matches) {
   videoToggleBtn.addEventListener("click", () => {
     videoToggleBtn.style.display = "none";
   });
 }
+
+// Pause video when scrolling out of view
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        enzyVideo.pause();
+        videoToggleBtn.style.opacity = "1";
+        playIconWrapper.style.display = "flex";
+        pauseIconWrapper.style.display = "none";
+      }
+    });
+  },
+  {
+    threshold: 0.5, // Trigger when 50% of the video is out of view
+  }
+);
+
+observer.observe(enzyVideo);
